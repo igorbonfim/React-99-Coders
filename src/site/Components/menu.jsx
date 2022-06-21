dda7b627d66ff2e936e290e082351e2f9f7199db
Igor Bonfim <igorbonfim88@gmail.com>
2022-06-21 16:44:28 -0300

    Ajuste no toggle do menu

--- src/site/Components/menu.jsx
+++ src/site/Components/menu.jsx
@@ -13,11 +13,13 @@ function Menu() {
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"          
+          aria-expanded="false" aria-label="Toggle navigation"
         >
-          <span className="nav-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse-navbar-collapse" id="navbarSupportedContent">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <a className="nav-link" aria-current="page" href="#banner">
