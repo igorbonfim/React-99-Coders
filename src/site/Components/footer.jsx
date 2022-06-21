2855c115ce3490dc1f42a09061260e319632cff7
Igor Bonfim <igorbonfim88@gmail.com>
2022-06-20 22:37:01 -0300

    Finalizando o site do CRM

---
+++ src/site/Components/footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Footer() {
+  var ano = new Date().getFullYear();
+
+  return (
+    <section id="footer">
+
+      <div>
+        <ul class="list-unstyled list-inline social text-center">
+            <li class="list-inline-item"><a href="#"><i class="fa fa-facebook fa-2x"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fa fa-twitter fa-2x"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fa fa-instagram fa-2x"></i></a></li>
+            <li class="list-inline-item"><a href="#" target="_blank"><i class="fa fa-envelope fa-2x"></i></a></li>
+        </ul>
+      </div>
+      <a href="mailto:igor@developer.com.br">igor@developer.com.br</a>
+      <p>Desenvolvido por Igor Bonfim - {ano}</p>
+
+    </section>
+  );
+}
+
+export default Footer;
--- src/site/site.jsx
+++ src/site/site.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import Menu from './Components/menu';
+import React from "react";
+import Menu from "./Components/menu";
+import Banner from "./Components/banner";
+import Features from "./Components/features";
+import Testemunho from "./Components/testemunho";
+import Precos from "./Components/precos";
+import Footer from "./Components/footer";
 
-function Site(){
-    return <div>
-        <Menu />
+function Site() {
+  return (
+    <div>
+      <Menu />
+      <Banner />
+      <Features />
+      <Testemunho />
+      <Precos />
+      <Footer />
     </div>
+  );
 }
 export default Site;
