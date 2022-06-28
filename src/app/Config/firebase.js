8033fea2fc7d812aac4a3d2d21f07b827e0cabff
Igor Bonfim <igorbonfim88@gmail.com>
2022-06-28 17:58:16 -0300

    Criação do backend com firebase e hooks

---
+++ src/app/Config/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAh9ATGEvl6iIGMtPSpt58G08_e9jXYdLo",
+    authDomain: "crm-feb8e.firebaseapp.com",
+    projectId: "crm-feb8e",
+    storageBucket: "crm-feb8e.appspot.com",
+    messagingSenderId: "160392836731",
+    appId: "1:160392836731:web:ec1e497e09942d016614de"
+  };
+  
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
--- src/app/Login/login.jsx
+++ src/app/Login/login.jsx
@@ -1,29 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './login.css';
 
+import firebase from '../Config/firebase';
+import 'firebase/compat/auth';
+
 function Login(){
+
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [sucesso, setSucesso] = useState('');
+
+    function LoginUsuario(){
+        firebase.auth().signInWithEmailAndPassword(email, senha)
+            .then(function(firebaseUser){
+                setSucesso('S');
+            })
+            .catch(function(error){
+                setSucesso('N');
+            })        
+    }
+
+    function alterarEmail(event){
+        setEmail(event.target.value);
+    }
+
+    function alterarSenha(event){
+        setSenha(event.target.value);
+    }
+
     return <div className="d-flex align-items-center text-center form-container">
         <form className="form-signin">
             <img className="mb-4" src="Images/logo-small2.png" alt="" />
             <h1 className="h3 mb-3 fw-normal">Login</h1>
 
             <div className="form-floating">
-                <input type="email" className="form-control" id="floatingInput" placeholder="E-mail" />
+                <input onChange={alterarEmail} type="email" className="form-control" id="floatingInput" placeholder="E-mail" />
                 <label for="floatingInput">E-mail</label>
             </div>
 
             <div className="form-floating">
-                <input type="password" className="form-control" id="floatingPassword" placeholder="Senha" />
+                <input onChange={alterarSenha} type="password" className="form-control" id="floatingPassword" placeholder="Senha" />
                 <label for="floatingPassword">Senha</label>
             </div>            
-            <button className="w-100 btn btn-lg btn-primary" type="submit">Acessar</button>
+            <button onClick={LoginUsuario} className="w-100 btn btn-lg btn-primary" type="button">Acessar</button>
 
+            { sucesso === 'N' ? <div className="alert alert-danger mt-2" role="alert"> E-mail ou senha inválida.</div> : null }
+           
             <div className="login-links mt-5">
                 <Link to="/app/resetsenha" className="mx-3">Esqueci minha senha</Link>                
                 <Link to="/app/novaconta" className="mx-3">Criar uma Conta</Link>
-            </div>
-
+            </div> 
+            
             <p className="mt-5 mb-3 text-muted">&copy; Desenvolvido por Igor Bonfim</p>
         </form>
     </div>
